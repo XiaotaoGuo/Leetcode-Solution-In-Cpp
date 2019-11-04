90b0437bb1a6ee92d5c37083ad78869385005432
XiaotaoGuo <xtguo1995@outlook.com>
2019-11-03 18:41:47 -0800

    update

---
+++ 437.path-sum-iii.cpp
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=437 lang=cpp
+ *
+ * [437] Path Sum III
+ *
+ * https://leetcode.com/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Easy (43.77%)
+ * Likes:    2335
+ * Dislikes: 146
+ * Total Accepted:    131.5K
+ * Total Submissions: 298.3K
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * You are given a binary tree in which each node contains an integer value.
+ * 
+ * Find the number of paths that sum to a given value.
+ * 
+ * The path does not need to start or end at the root or a leaf, but it must go
+ * downwards
+ * (traveling only from parent nodes to child nodes).
+ * 
+ * The tree has no more than 1,000 nodes and the values are in the range
+ * -1,000,000 to 1,000,000.
+ * 
+ * Example:
+ * 
+ * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ * 
+ * ⁠     10
+ * ⁠    /  \
+ * ⁠   5   -3
+ * ⁠  / \    \
+ * ⁠ 3   2   11
+ * ⁠/ \   \
+ * 3  -2   1
+ * 
+ * Return 3. The paths that sum to 8 are:
+ * 
+ * 1.  5 -> 3
+ * 2.  5 -> 2 -> 1
+ * 3. -3 -> 11
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        int ans = 0;
+        bool flag = true;
+        dfs(root, sum, ans, flag);
+        return ans;
+    }
+private:
+    void dfs(TreeNode* node, int sum, int& ans, bool flag){
+        if(!node) return;
+        if(sum == node->val){
+            ans++;
+        }
+        if(flag){
+            dfs(node->left, sum, ans, true);
+            dfs(node->right, sum, ans, true);
+        }
+        dfs(node->left, sum - node->val, ans, false);
+        dfs(node->right, sum - node->val, ans, false);
+        
+    }
+};
+// @lc code=end
+
