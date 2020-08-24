df012522f7e1ff744cd961ccd364aa9deb3ffea1
XiaotaoGuo <guoxiaotao0819@gmail.com>
2020-08-24 09:27:48 -0400

    404 added

---
+++ tree/easy/404.SumOfLeftLeaves.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=404 lang=cpp
+ *
+ * [404] Sum of Left Leaves
+ *
+ * https://leetcode.com/problems/sum-of-left-leaves/description/
+ *
+ * algorithms
+ * Easy (50.87%)
+ * Likes:    1294
+ * Dislikes: 132
+ * Total Accepted:    192.7K
+ * Total Submissions: 375.2K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Find the sum of all left leaves in a given binary tree.
+ * 
+ * Example:
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * There are two left leaves in the binary tree, with values 9 and 15
+ * respectively. Return 24.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        dfs(root, false, sum);
+        return sum;
+    }
+    
+private:
+    void dfs(TreeNode* node, bool is_left, int& sum) {
+        if (!node) return;
+        if (!node->left && !node->right) sum += is_left ? node->val : 0;
+        dfs(node->left, true, sum);
+        dfs(node->right, false, sum);
+    }
+};
+// @lc code=end
+
