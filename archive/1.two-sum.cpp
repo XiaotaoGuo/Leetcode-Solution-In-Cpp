08fb75c45050d1a90b883b44ed810c446cf4d7ee
XiaotaoGuo <guoxiaotao0819@gmail.com>
2020-10-04 16:54:51 -0400

    format some files

--- archive/1.two-sum.cpp
+++ archive/1.two-sum.cpp
@@ -15,41 +15,39 @@
  *
  * Given an array of integers, return indices of the two numbers such that they
  * add up to a specific target.
- * 
+ *
  * You may assume that each input would have exactly one solution, and you may
  * not use the same element twice.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Given nums = [2, 7, 11, 15], target = 9,
- * 
+ *
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
-#include<vector>
-#include<unordered_map>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-       unordered_map<int, int> hmap;
-        for(int i = 0; i < nums.size(); ++i){
-            int num = nums[i];
-            if(hmap.find(target-num) == hmap.end()){
-                hmap[num] = i;
-            }
-            else{
-                return vector<int>{hmap[target-num], i};
-            }
-        }
-        return {}; 
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> hmap;
+    for (int i = 0; i < nums.size(); ++i) {
+      int num = nums[i];
+      if (hmap.find(target - num) == hmap.end()) {
+        hmap[num] = i;
+      } else {
+        return vector<int>{hmap[target - num], i};
+      }
     }
+    return {};
+  }
 };
 // @lc code=end
-
--- archive/3.longest-substring-without-repeating-characters.cpp
+++ archive/3.longest-substring-without-repeating-characters.cpp
@@ -15,72 +15,74 @@
  *
  * Given a string, find the length of the longest substring without repeating
  * characters.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: "abcabcbb"
- * Output: 3 
- * Explanation: The answer is "abc", with the length of 3. 
- * 
- * 
- * 
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: "bbbbb"
  * Output: 1
  * Explanation: The answer is "b", with the length of 1.
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: "pwwkew"
  * Output: 3
- * Explanation: The answer is "wke", with the length of 3. 
+ * Explanation: The answer is "wke", with the length of 3.
  * ⁠            Note that the answer must be a substring, "pwke" is a
  * subsequence and not a substring.
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  */
 
 // @lc code=start
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
-public:
+   public:
     int lengthOfLongestSubstring(string s) {
-                if(!s.length()) return 0;
+        if (!s.length()) return 0;
         int ans = 0;
         int l = 0, r = 0;
         bool flags[128];
-        memset(flags, 0, 120*sizeof(bool));
+        memset(flags, 0, 120 * sizeof(bool));
         int currLength = 0;
-        while(r < s.length()){
-            // keep right shift right point until we found a char discovered before or reach the end of string
-            while(r < s.length() && !flags[s[r] - ' ']){
-                flags[s[r]-' '] = true;
+        while (r < s.length()) {
+            // keep right shift right point until we found a char discovered
+            // before or reach the end of string
+            while (r < s.length() && !flags[s[r] - ' ']) {
+                flags[s[r] - ' '] = true;
                 r++;
                 currLength++;
             }
             ans = currLength > ans ? currLength : ans;
-            // if haven't reach the end of string, we can right shift the left ptr to start next run
-            if(r < s.length()){
-                while(s[l] != s[r]){
-                    flags[s[l]-' '] = false; // reset the flag
+            // if haven't reach the end of string, we can right shift the left
+            // ptr to start next run
+            if (r < s.length()) {
+                while (s[l] != s[r]) {
+                    flags[s[l] - ' '] = false;  // reset the flag
                     l++;
                     currLength--;
                 }
                 l++;
-                //currLength--;
+                // currLength--;
             }
             r++;
         }
@@ -89,4 +91,3 @@ public:
     }
 };
 // @lc code=end
-
