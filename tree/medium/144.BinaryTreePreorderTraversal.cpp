c896155928148af80f58ff6c44e110514fc64252
XiaotaoGuo <guoxiaotao0819@gmail.com>
2020-10-08 19:14:59 -0400

    144 added

---
+++ tree/medium/144.BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=144 lang=cpp
+ *
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (56.20%)
+ * Likes:    1770
+ * Dislikes: 65
+ * Total Accepted:    541.9K
+ * Total Submissions: 962.2K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the preorder traversal of its nodes'
+ * values.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,2,3]
+ * Output: [1,2,3]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [1]
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: root = [1,2]
+ * Output: [1,2]
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: root = [1,null,2]
+ * Output: [1,2]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * Recursive solution is trivial, could you do it iteratively?
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+#include <stack>
+
+class Solution {
+public:
+    std::vector<int> preorderTraversal(TreeNode* root) {
+        std::vector<int> result;
+        if (!root) return result;
+        std::stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode* curr = s.top();
+            s.pop();
+            result.push_back(curr->val);
+            if (curr->right) s.push(curr->right);
+            if (curr->left) s.push(curr->left);
+        }
+        return result;
+    }
+};
+// @lc code=end
+
