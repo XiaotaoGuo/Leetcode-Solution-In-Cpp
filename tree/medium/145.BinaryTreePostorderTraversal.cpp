43b71d76491275487a4d40c5a00d4dbf175c3b10
XiaotaoGuo <guoxiaotao0819@gmail.com>
2020-10-10 20:13:30 -0400

    145 added

---
+++ tree/medium/145.BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode id=145 lang=cpp
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Medium (55.73%)
+ * Likes:    2082
+ * Dislikes: 104
+ * Total Accepted:    415.3K
+ * Total Submissions: 742.6K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the postorder traversal of its
+ * nodes' values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [3,2,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1]
+ * Output: [1]
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: root = [1,2]
+ * Output: [2,1]
+ *
+ *
+ * Example 5:
+ *
+ *
+ * Input: root = [1,null,2]
+ * Output: [2,1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ *
+ * Follow up:
+ *
+ * Recursive solution is trivial, could you do it iteratively?
+ *
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+#include <stack>
+#include <vector>
+class Solution {
+public:
+    std::vector<int> postorderTraversal(TreeNode* root) {
+        std::vector<int> result;
+
+        dfs(root, result);
+
+        return result;
+    }
+
+private:
+    void dfs(TreeNode* root, std::vector<int>& result) {
+        if (!root) return;
+        dfs(root->left, result);
+        dfs(root->right, result);
+        result.push_back(root->val);
+    }
+};
+// @lc code=end
