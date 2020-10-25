80cbb80dadcb1e2c804b3de8da9b63a884ece727
XiaotaoGuo <guoxiaotao0819@gmail.com>
2020-10-25 19:09:28 -0400

    110 added

---
+++ tree/easy/110.BalancedBinaryTree.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=110 lang=cpp
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (43.78%)
+ * Likes:    2705
+ * Dislikes: 186
+ * Total Accepted:    487.6K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ * For this problem, a height-balanced binary tree is defined as:
+ *
+ *
+ * a binary tree in which the left and right subtrees of every node differ in
+ * height by no more than 1.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,2,3,3,null,null,4,4]
+ * Output: false
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ * Output: true
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -10^4 <= Node.val <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root) return true;
+        return isBalanced(root->left) && isBalanced(root->right) &&
+               std::abs(height(root->left) - height(root->right)) <= 1;
+    }
+
+private:
+    int height(TreeNode* root) {
+        if (!root) return 0;
+        if (!root->left && !root->right) return 1;
+        return std::max(height(root->left), height(root->right)) + 1;
+    }
+};
+// @lc code=end
