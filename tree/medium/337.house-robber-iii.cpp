d96bef45dfa375fedab04f64b9038771c36308f4
郭晓涛 <guoxiaotao@kakus-MacBook-Pro.local>
2020-10-27 11:58:59 -0400

    337 added

---
+++ tree/medium/337.house-robber-iii.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=337 lang=cpp
+ *
+ * [337] House Robber III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        if (!root) return 0;
+        if (hmap.find(root) != hmap.end()) return hmap[root];
+        int curr_sum = root->val;
+        if (root->left) {
+            curr_sum += rob(root->left->left);
+            curr_sum += rob(root->left->right);
+        }
+        if (root->right) {
+            curr_sum += rob(root->right->left);
+            curr_sum += rob(root->right->right);
+        }
+                            
+        int prev_sum = rob(root->left) + rob(root->right);
+        hmap[root] = max(curr_sum, prev_sum);
+        return hmap[root];
+    }
+
+private:
+    std::unordered_map<TreeNode*, int> hmap;
+};
+// @lc code=end
+
