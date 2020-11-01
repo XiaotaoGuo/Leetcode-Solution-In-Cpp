d49cd379def422ae9472062775446b08e4ff4079
XiaotaoGuo <xtguo1995@Outlook.com>
2020-11-01 00:40:21 -0400

    98 added

---
+++ tree/medium/98.ValidateBinarySearchTree.cpp
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (28.07%)
+ * Likes:    4735
+ * Dislikes: 585
+ * Total Accepted:    798.3K
+ * Total Submissions: 2.8M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * 
+ * Assume a BST is defined as follows:
+ * 
+ * 
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 
+ * Input: [2,1,3]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * 
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <limits.h>
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (root == nullptr) return true;
+        if (!isValidBST(root->left)) return false;
+        if (prev_val >= root->val && !is_first) return false;
+        prev_val = root->val;
+        is_first = false;
+        return isValidBST(root->right);
+
+    }
+
+private:
+    int prev_val = INT_MIN;
+    bool is_first = true;
+};
+// @lc code=end
+
