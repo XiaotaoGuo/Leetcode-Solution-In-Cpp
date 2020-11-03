028989314773e7c4e71c768247ef67e5e4afd53c
XiaotaoGuo <xtguo1995@Outlook.com>
2020-11-02 22:10:43 -0500

    235 added

---
+++ tree/easy/235.LowestCommonAncestorOfABinarySearchTree.cpp
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode id=235 lang=cpp
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (50.71%)
+ * Likes:    2481
+ * Dislikes: 112
+ * Total Accepted:    443.4K
+ * Total Submissions: 873.5K
+ * Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of
+ * two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of
+ * itself).”
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant
+ * of itself according to the LCA definition.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [2,1], p = 2, q = 1
+ * Output: 2
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [2, 10^5].
+ * -10^9 <= Node.val <= 10^9
+ * All Node.val are unique.
+ * p != q
+ * p and q will exist in the BST.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == p || root == q) return root;
+        if (p->val < q->val) {
+            if (p->val < root->val && root->val < q->val)
+                return root;
+            else if (q->val < root->val) {
+                return lowestCommonAncestor(root->left, p, q);
+            } else {
+                return lowestCommonAncestor(root->right, p, q);
+            }
+        } else {
+            if (p->val > root->val && root->val > q->val) {
+                return root;
+            } else if (p->val < root->val) {
+                return lowestCommonAncestor(root->left, p, q);
+            } else {
+                return lowestCommonAncestor(root->right, p, q);
+            }
+        }
+    }
+};
+// @lc code=end
