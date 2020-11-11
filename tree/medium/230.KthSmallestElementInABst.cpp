757396fea26380e3f189aff0a74bc4d60d6a0193
XiaotaoGuo <xtguo1995@Outlook.com>
2020-11-11 16:48:53 -0500

    230

---
+++ tree/medium/230.KthSmallestElementInABst.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=230 lang=cpp
+ *
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (61.33%)
+ * Likes:    3137
+ * Dislikes: 75
+ * Total Accepted:    460K
+ * Total Submissions: 747.9K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given a binary search tree, write a function kthSmallest to find the kth
+ * smallest element in it.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * Output: 1
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * Output: 3
+ *
+ *
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to
+ * find the kth smallest frequently? How would you optimize the kthSmallest
+ * routine?
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of elements of the BST is between 1 to 10^4.
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) { return dfs(root, k); }
+
+private:
+    int dfs(TreeNode* root, int& k) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int val = kthSmallest(root->left, k);
+        if (k == 0) return val;
+        k--;
+        if (k == 0) return root->val;
+        return kthSmallest(root->right, k);
+    }
+};
+// @lc code=end
