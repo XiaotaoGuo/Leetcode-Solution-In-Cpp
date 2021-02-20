cdada62b05430995394d040023756a47eeb4cc30
XiaotaoGuo <xtguo1995@outlook.com>
2021-02-20 01:00:36 -0500

    19 added

---
+++ linked-list/medium/19.RemoveNthNodeFromEndOfList.cpp
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=19 lang=cpp
+ *
+ * [19] Remove Nth Node From End of List
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (35.71%)
+ * Likes:    4772
+ * Dislikes: 288
+ * Total Accepted:    794.8K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given the head of a linked list, remove the n^th node from the end of the
+ * list and return its head.
+ * 
+ * Follow up: Could you do this in one pass?
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1], n = 1
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is sz.
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+        int step = 0;
+        while (step < n) {
+            fast = fast->next;
+            step++;
+        }
+        
+        if (!fast) return head->next;
+
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return dummy->next;
+    }
+};
+// @lc code=end
+
