b8f96570ffaeb73baf75ffb5cebef225744626c5
XiaotaoGuo <xtguo1995@outlook.com>
2021-02-22 19:49:27 -0500

    82 added

---
+++ linked-list/medium/82.RemoveDuplicatesFromSortedListIi.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=82 lang=cpp
+ *
+ * [82] Remove Duplicates from Sorted List II
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
+ *
+ * algorithms
+ * Medium (39.18%)
+ * Likes:    2692
+ * Dislikes: 122
+ * Total Accepted:    315.7K
+ * Total Submissions: 805K
+ * Testcase Example:  '[1,2,3,3,4,4,5]'
+ *
+ * Given the head of a sorted linked list, delete all nodes that have duplicate
+ * numbers, leaving only distinct numbers from the original list. Return the
+ * linked list sorted as well.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,3,4,4,5]
+ * Output: [1,2,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,1,1,2,3]
+ * Output: [2,3]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is in the range [0, 300].
+ * -100 <= Node.val <= 100
+ * The list is guaranteed to be sorted in ascending order.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+
+        ListNode *curr = head, *prev = dummy;
+        while (curr) {
+            bool is_distinct = true;
+            while (curr->next && curr->val == curr->next->val) {
+                curr = curr->next;
+                is_distinct = false;
+            }
+            if (!is_distinct) {
+                prev->next = curr->next;
+                curr = prev->next;
+            }
+            else {
+                prev = prev->next;
+                curr = prev->next;
+            }
+
+        }
+
+        return dummy->next;
+    }
+};
+// @lc code=end
+
