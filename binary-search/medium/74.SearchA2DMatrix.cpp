1f9c46511f296fc4a8469a6744eed449e422657e
XiaotaoGuo <xtguo1995@outlook.com>
2021-03-31 20:54:07 -0400

    74 added

---
+++ binary-search/medium/74.SearchA2DMatrix.cpp
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=74 lang=cpp
+ *
+ * [74] Search a 2D Matrix
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix/description/
+ *
+ * algorithms
+ * Medium (37.66%)
+ * Likes:    3092
+ * Dislikes: 194
+ * Total Accepted:    433.6K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,60]]\n3'
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ *
+ *
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the
+ * previous row.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 100
+ * -10^4 <= matrix[i][j], target <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+#include <vector>
+
+class Solution {
+public:
+    bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int left = 0;
+        int right = m - 1;
+        while (left < right) {
+            int mid = (left + right) / 2 + 1;
+            if (matrix[mid][0] > target) {
+                right = mid - 1;
+            } else if (matrix[mid][0] < target) {
+                left = mid;
+            } else {
+                return true;
+            }
+        }
+
+        int row = left;
+        left = 0;
+        right = n - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (matrix[row][mid] > target) {
+                right = mid - 1;
+            } else if (matrix[row][mid] < target) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return matrix[row][left] == target;
+    }
+};
+// @lc code=end
