171d66a74a701507bf526a9eec8c5e80d90a312f
XiaotaoGuo <xtguo1995@outlook.com>
2021-05-12 22:49:18 -0400

    136 added

---
+++ bit-manipulation/easy/136.SingleNumber.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=136 lang=cpp
+ *
+ * [136] Single Number
+ *
+ * https://leetcode.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (66.85%)
+ * Likes:    6237
+ * Dislikes: 201
+ * Total Accepted:    1.2M
+ * Total Submissions: 1.7M
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers nums, every element appears twice except
+ * for one. Find that single one.
+ * 
+ * You must implement a solution with a linear runtime complexity and use only
+ * constant extra space.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [2,2,1]
+ * Output: 1
+ * Example 2:
+ * Input: nums = [4,1,2,1,2]
+ * Output: 4
+ * Example 3:
+ * Input: nums = [1]
+ * Output: 1
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 3 * 10^4
+ * -3 * 10^4 <= nums[i] <= 3 * 10^4
+ * Each element in the array appears twice except for one element which appears
+ * only once.
+ * 
+ * 
+ */
+
+// @lc code=start
+#include <vector>
+
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) {
+        int val = 0;
+        for (int num: nums) {
+            val ^= num;            
+        }
+
+        return val;
+    }
+};
+// @lc code=end
+
