1191649dcebd70d65c0bb32482736d561803a484
XiaotaoGuo <xtguo1995@outlook.com>
2021-06-01 19:39:22 -0400

    268

---
+++ bit-manipulation/easy/268.MissingNumber.cpp
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode id=268 lang=cpp
+ *
+ * [268] Missing Number
+ *
+ * https://leetcode.com/problems/missing-number/description/
+ *
+ * algorithms
+ * Easy (55.80%)
+ * Likes:    3124
+ * Dislikes: 2558
+ * Total Accepted:    685.9K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,0,1]'
+ *
+ * Given an array nums containing n distinct numbers in the range [0, n],
+ * return the only number in the range that is missing from the array.
+ * 
+ * Follow up: Could you implement a solution using only O(1) extra space
+ * complexity and O(n) runtime complexity?
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [3,0,1]
+ * Output: 2
+ * Explanation: n = 3 since there are 3 numbers, so all numbers are in the
+ * range [0,3]. 2 is the missing number in the range since it does not appear
+ * in nums.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [0,1]
+ * Output: 2
+ * Explanation: n = 2 since there are 2 numbers, so all numbers are in the
+ * range [0,2]. 2 is the missing number in the range since it does not appear
+ * in nums.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * Explanation: n = 9 since there are 9 numbers, so all numbers are in the
+ * range [0,9]. 8 is the missing number in the range since it does not appear
+ * in nums.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: nums = [0]
+ * Output: 1
+ * Explanation: n = 1 since there is 1 number, so all numbers are in the range
+ * [0,1]. 1 is the missing number in the range since it does not appear in
+ * nums.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == nums.length
+ * 1 <= n <= 10^4
+ * 0 <= nums[i] <= n
+ * All the numbers of nums are unique.
+ * 
+ * 
+ */
+
+// @lc code=start
+#include <vector>
+
+class Solution {
+public:
+    int missingNumber(std::vector<int>& nums) {
+        int mask = 0;
+        for (int i = 0; i <= nums.size(); ++i) {
+            mask ^= i;
+        }
+
+        for (int num : nums) {
+            mask ^= num;
+        }
+
+        return mask;
+    }
+};
+// @lc code=end
+
